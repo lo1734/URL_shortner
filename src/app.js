a5ad8ed220e2812ce00e870191eb8ba0aa0fd5d1
lohitaksha <lohi1734@gmail.com>
2026-06-22 18:03:25 +0530

    fix: change wildcard route to (.*) for Express 5 compatibility

--- src/app.js
+++ src/app.js
@@ -59,7 +59,7 @@ app.use('/auth',authRoutes);
 app.use('/analytics', analyticsRoutes)
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
